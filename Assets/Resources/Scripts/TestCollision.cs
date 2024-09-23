a809edf250d18daa5f30996d74e15d8340957297
선재영 <wodud1892@naver.com>
2024-09-23 14:58:39 +0900

    ✨ update : Camera

--- Assets/Resources/Scripts/TestCollision.cs
+++ Assets/Resources/Scripts/TestCollision.cs
@@ -1,47 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class TestCollision : MonoBehaviour
-{
+// public class TestCollision : MonoBehaviour
+// {
 
-    void OnCollisionEnter(Collision collisionInfo)
-    {
-        if(collisionInfo.gameObject.name != "Plane")
-            Debug.Log($"Collision @ {collisionInfo.gameObject.name}");
-    }
+//     void OnCollisionEnter(Collision collisionInfo)
+//     {
+//         if(collisionInfo.gameObject.name != "Plane")
+//             Debug.Log($"Collision @ {collisionInfo.gameObject.name}");
+//     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log($"isTrigger @ {other.gameObject.name}");
-    }
+//     void OnTriggerEnter(Collider other)
+//     {
+//         Debug.Log($"isTrigger @ {other.gameObject.name}");
+//     }
     
-    void Update()
-    {
-        // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); //뷰 포인트
-        //로컬에서 월드 방향으로 변환
-        // Vector3 look = transform.TransformDirection(Vector3.forward);
-        // Debug.DrawRay(transform.position+ Vector3.up, look * 10, Color.red);
+//     void Update()
+//     {
+//         // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); //뷰 포인트
+//         //로컬에서 월드 방향으로 변환
+//         // Vector3 look = transform.TransformDirection(Vector3.forward);
+//         // Debug.DrawRay(transform.position+ Vector3.up, look * 10, Color.red);
 
-        // RaycastHit hit;
-        // if(Physics.Raycast(transform.position, look ,out hit, 10))
-        // {
-        //     Debug.Log($"Raycast {hit.collider.gameObject.name}");
-        // }
+//         // RaycastHit hit;
+//         // if(Physics.Raycast(transform.position, look ,out hit, 10))
+//         // {
+//         //     Debug.Log($"Raycast {hit.collider.gameObject.name}");
+//         // }
 
-        if(Input.GetMouseButton(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//         if(Input.GetMouseButton(0))
+//         {
+//             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f , Color.red , 1.0f);
+//             Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f , Color.red , 1.0f);
 
-            int mask = (1<< 8) | (1 << 7);
+//             int mask = (1<< 8) | (1 << 7);
 
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 100.0f , mask))
-            {
-                Debug.Log($"Raycast {hit.collider.gameObject.name}");
-            }
-        }
-    }
-}
+//             RaycastHit hit;
+//             if(Physics.Raycast(ray, out hit, 100.0f , mask))
+//             {
+//                 Debug.Log($"Raycast {hit.collider.gameObject.name}");
+//             }
+//         }
+//     }
+// }
